99ae097577e39076fb537a48311138a4d91f77cc
RomanSaiko <rsaiko1995@gmail.com>
2019-11-20 22:39:39 +0200

    added three functions

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 alert('Please check console');
 
-function getTruckIdsCallback(callback) {
+function getTrucktruckIdCallback(callback) {
     setTimeout(() => {
         callback([1,2,5,9,67]);
 }, 1000)
 }
 
-function getTruckIds() {
+function getTrucktruckId() {
     return new Promise((resolve => {
-        getTruckIdsCallback(result => resolve(result));
+        getTrucktruckIdCallback(result => resolve(result));
 }));
 }
 
@@ -25,23 +25,59 @@ function getTruckByIdCallback(id, callback) {
 })
 }
 
-function getTruckById() {
+function getTruckById(id) {
     //Please implemented this method use getTruckByIdCallback
-
+    return new Promise((resolve, reject) => {
+        getTruckByIdCallback(id, (result, err) => {
+        if (err) reject(err);
+        else resolve(result);
+    });
+});
 }
 
 // callback(list, err)
 // list - list of trucks
 function getTruckListCallback(callback) {
-
+    getTrucktruckId()
+        .then(res => {
+        const truckList = [];
+        Object.keys(res).map(key => getTruckById(res[key], 3).then(res => truckList.push(res)));
+        return truckList;
+    })
+    .then(truckList => callback(truckList))
 }
 
+getTruckListCallback(function (truckList) {
+    console.log(truckList);
+});
+// callback
+
+//Promise
 function getTruckListPromise() {
     return new Promise(((resolve, reject) => {
-
+        getTrucktruckId()
+            .then(res => {
+                const truckList = [];
+                Object.keys(res).map(key => getTruckById(res[key], 3).then(res => truckList.push(res)));
+                return truckList;
+            })
+            .then(truckList => resolve(truckList))
     }))
 }
 
-async function getTruckListAsynAwait() {
+getTruckListPromise().then(res => console.log(res));
+//Promise
 
+//async-await
+async function getTruckListAsynAwait() {
+    const truckId = await getTrucktruckId();
+    const truckList = [];
+    Object.keys(truckId).map(async (key) => {
+        const truckItem = await getTruckById(truckId[key], 3);
+        truckList.push(truckItem);
+    });
+    return truckList;
 }
+
+getTruckListAsynAwait().then(res => console.log(res));
+//async-await
